506e1cc932e21b8e95d0edf43483daac8218233f
VJNap <vjnapolitano1364@gmail.com>
2020-02-04 20:15:38 -0600

    VN - Project Folder Structure, Google Map w/ geolocation/markers, and OrgHunter API data.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,69 @@
 // ========== API Keys ==========
 const googleAPIkey = 'AIzaSyB3_355xgTrbvLb3K_FE_2bpig4WBtCGgM';
 const charityAPIkey = '3ea2f1ef16ab9b240050c2cf1c055650';
-// ========== API Keys ==========
+// ========== CORS Fix ==========
+jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+// ========== Query Variables ==========
+// let name = user input
+// let city = user input
+// let state = user input 
+// let category = user input
+// let responseNum = user input
+let queryURL = "http://data.orghunter.com/v1/charitysearch?user_key=" + charityAPIkey + "&eligible=1&rows=100"
+// ========== Charity Ajax Call ==========
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response.data);
+    let orgLat;
+    let orgLon;
+    for (i = 0; i < response.data.length; i++) {
+        console.log('==========');
+        console.log(response.data[i].charityName);
+        console.log(response.data[i].url);
+        console.log(response.data[i].donationUrl);
+        console.log(response.data[i].city + ', ' + response.data[i].state);
+        console.log(response.data[i].latitude);
+        console.log(response.data[i].longitude);
+        console.log(response.data[i].missionStatement);
+        // ========== Map Marker GeoLocation Variables ==========
+        orgLat = response.data[i].latitude;
+        orgLon = response.data[i].longitude;
+        console.log(orgLat + ', ' + orgLon);
+    };
+    initMap(orgLat, orgLon);
+});
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 var map, infoWindow;
-function initMap() {
-    var myLatLng = { lat: 41.8781, lng: -87.6298 };
+function initMap(orgLat, orgLon) {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
+        center: { lat: -34.397, lng: 150.644 },
         zoom: 6
     });
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-    });
     infoWindow = new google.maps.InfoWindow;
-    // Try HTML5 geolocation.
+    // ========== Google Maps API Marker Test ----------
+    let myLatlng = new google.maps.LatLng(orgLat, orgLon);
+    let marker = new google.maps.Marker({
+        position: myLatlng,
+        title: "Hello World!"
+    });
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
+    // ========== Try HTML5 geolocation ==========
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
-
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
@@ -38,20 +75,12 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
-};
+}
+// ========== Google API Geolocation (DO NOT TOUCH) ==========
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
+        'Error: Your browser does not support geolocation.');
     infoWindow.open(map);
-};
-// ========== Charity Ajax Call ==========
-// let queryURL = "http://data.orghunter.com/v1/charitysearch?user_key=" + charityAPIkey; + "&eligible=1"
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response);
-// });
+}
