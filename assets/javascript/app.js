9be03b27033b7bb7f2e4fe1afa3ad603e1d8af30
VJNap <vjnapolitano1364@gmail.com>
2020-02-06 19:07:39 -0600

    VN - Pushing marker changes to set master file for redistribution.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ jQuery.ajaxPrefilter(function (options) {
 // let state = user input 
 // let category = user input
 // let responseNum = user input
-let queryURL = "http://data.orghunter.com/v1/charitysearch?user_key=" + charityAPIkey + "&eligible=1&rows=100"
+let queryURL = "http://data.orghunter.com/v1/charitysearch?user_key=" + charityAPIkey + "&eligible=1&city=Chicago"
 // ========== Charity Ajax Call ==========
 $.ajax({
     url: queryURL,
@@ -22,7 +22,9 @@ $.ajax({
     console.log(response.data);
     let orgLat;
     let orgLon;
+    let orgName;
     for (i = 0; i < response.data.length; i++) {
+        // ========== Response Log ==========
         console.log('==========');
         console.log(response.data[i].charityName);
         console.log(response.data[i].url);
@@ -31,19 +33,28 @@ $.ajax({
         console.log(response.data[i].latitude);
         console.log(response.data[i].longitude);
         console.log(response.data[i].missionStatement);
+        // ========== MVP Display ==========
+        $('#displayDiv').append('==============================' + '<br>'
+            + 'Charity: ' + response.data[i].charityName +
+            '<br>' + '<a href=' + response.data[i].url + '>' + 'Get Info</a>' +
+            '<br>' + '<a href=' + response.data[i].donationUrl + '>' + 'Donate</a>' +
+            '<br>' + 'Location: ' + response.data[i].city + ', ' + response.data[i].state +
+            '<br>' + 'Mission Statement: ' + response.data[i].missionStatement +
+            '<br>');
         // ========== Map Marker GeoLocation Variables ==========
         orgLat = response.data[i].latitude;
         orgLon = response.data[i].longitude;
+        orgName = response.data[i].charityName;
         console.log(orgLat + ', ' + orgLon);
     };
-    initMap(orgLat, orgLon);
+    initMap(orgLat, orgLon, orgName);
 });
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 var map, infoWindow;
-function initMap(orgLat, orgLon) {
+function initMap(orgLat, orgLon, orgName) {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 6
@@ -53,7 +64,7 @@ function initMap(orgLat, orgLon) {
     let myLatlng = new google.maps.LatLng(orgLat, orgLon);
     let marker = new google.maps.Marker({
         position: myLatlng,
-        title: "Hello World!"
+        title: orgName
     });
     // To add the marker to the map, call setMap();
     marker.setMap(map);
